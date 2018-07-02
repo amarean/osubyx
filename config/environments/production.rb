6e8fdedb9494522b9eaf7e3dcf6516d70a2d6c9f
amarean <marean.1@osu.edu>
2018-07-01 22:43:01 -0400

    removed rogue _

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  #config.serve_static_assets = true
+   config.serve_static_assets = true
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -26,7 +26,7 @@ Rails.application.configure do
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
-_
+
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
 
