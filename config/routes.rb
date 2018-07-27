0c9db80094c08775e4124927710569c46b30b763
amarean <marean.1@osu.edu>
2018-07-27 17:44:36 -0400

    added route for sitemap

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
 
   get 'contact-me', to: 'messages#new', as: 'new_message'
   post 'contact-me', to: 'messages#create', as: 'create_message'
+  get 'sitemap', to: 'sitemap#index', constraints: { format: 'xml' }
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
